1a4edd854f92c11753b4bb7acfe5d7d6ce6b0923
OmarHaji0001 <s12029055@stu.najah.edu>
2025-12-17 14:47:14 +0200

    add static file with simple html

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,14 @@
 package org.example;
 
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 
-//import io.javalin.javalin
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Javalin app = Javalin.create();
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public", Location.CLASSPATH);
+        });
+
         app.start(7070);
         System.out.println("server started: http://localhost:7070");
     }
