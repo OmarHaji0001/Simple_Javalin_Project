bc288f008508b5bbcafd86d2a66d7200d6a339df
OmarHaji0001 <s12029055@stu.najah.edu>
2025-12-17 14:37:30 +0200

    main JL start, adding slf4 for the build warning

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,14 @@
 package org.example;
 
+import io.javalin.Javalin;
+
+//import io.javalin.javalin
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Javalin app = Javalin.create();
+        app.start(7070);
+        System.out.println("server started: http://localhost:7070");
     }
 }
